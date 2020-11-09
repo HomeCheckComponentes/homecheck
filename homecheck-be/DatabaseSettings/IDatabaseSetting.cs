31657b36df7139b48511ce30eece5d7ede31a467
Mariela  Bonilla <mbonilla.guti@gmail.com>
2020-11-09 04:20:49 -0600

    Agregar archivos de proyecto.

---
+++ homecheck-be/DatabaseSettings/IDatabaseSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homecheck_be.Service
+{
+    public interface IDatabaseSetting
+    {
+        string CollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
